57678a8065020a772fefad4d93cecd14c1050c1f
Christopher Friedt <cfri@pervices.com>
2018-03-21 20:35:21 +0000

    added basic unit tests. currently the source + sink one does not pass but the source one does

--- python/qa_crimson_source_sink_sob.py
+++ python/qa_crimson_source_sink_sob.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2017 <+YOU OR YOUR COMPANY+>.
-# 
+#
+# Copyright 2017 Per Vices Corporation.
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import math
 import time
+
+import threading
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 from gnuradio import uhd
 
-from crimson_sink import crimson_sink
-from crimson_source import crimson_source
+#from crimson_sink import crimson_sink
+#from crimson_source import crimson_source
 
 import numpy as np
 from scipy.signal import chirp, sweep_poly
 
-def gen_chirp( samp_rate = 10000000, A = 32000, f0 = 200, f1 = 20000, T = 1, phi0 = 0 ):
+def gen_chirp( samp_rate = 10000000, A = 1, f0 = 200, f1 = 20000, T = 1, phi0 = 0 ):
     """
-    
+
     generate a chirp signal
-    
+
     samp_rate := sample rate of the signal in Samples / s
     A         := Amplitude of chirp (constant)
     f0        := start frequency, Hz
     f1        := stop frequency, Hz
     T         := sweep time, s
-    
+
     See https://en.wikipedia.org/wiki/Chirp
     http://scipy-cookbook.readthedocs.io/items/FrequencySweptDemo.html
     """
-    
+
     if samp_rate <= 0:
         raise ValueError( 'samp_rate must be positive' )
 
@@ -62,13 +64,13 @@ def gen_chirp( samp_rate = 10000000, A = 32000, f0 = 200, f1 = 20000, T = 1, phi
         raise ValueError( 'T must be positive' )
 
     N = int( T * samp_rate )
-    
+
     if N <= 0:
         raise ValueError( 'The number of samples must be positive' )
 
-    t = np.linspace(0, T, N )
+    t = np.linspace(0, T, N ) 
     x = chirp( t, f0, f1, T, method='linear' )
-    
+
     return x
 
 class qa_crimson_source_sink_sob (gr_unittest.TestCase):
@@ -79,104 +81,107 @@ class qa_crimson_source_sink_sob (gr_unittest.TestCase):
     def tearDown (self):
         self.tb = None
 
-    def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+    # utility function for test_002
+    def tb_killer_fn( self, delay ):
+        #print( "tb_killer_fn(): sleep {0} s".format( delay ) )
+        time.sleep( delay )
+        #print( "tb_killer_fn(): stopping top block now".format( delay ) )
+        self.tb.stop()
 
-    def test_0phase_delay_5 (self):
-        
+    def test_000_num_samps_and_done_with_5s_sob (self):
+
         ##################################################
         # Variables
         ##################################################
-        sob = uhd.time_spec_t.get_system_time().get_real_secs() + 10
-        samp_rate = 10e6
-        freq = 2.4e9
-        gain = 20
+        samp_rate = 1e6
+        freq = 15e6
+        gain = 0
         channels = [0]
+        sob = 5
+        nsamples = int( samp_rate ) # 1 s worth of data
 
-        chirp_i = gen_chirp()
+        chirp_i = gen_chirp( samp_rate )
         chirp_q = np.empty( chirp_i.size )
         chirp = chirp_i + 1j * chirp_q
 
         ##################################################
         # Blocks
         ##################################################
-        csrc = crimson_source.make(
-                ",".join(("", "crimson:sob-host={0}".format( sob ))),
-#                 ",".join(("", "")),
-                uhd.stream_args(
-                        cpu_format = "sc16",
-                        otw_format = "sc16",
-                        channels = (channels),
-                )
-        )
-        csnk = crimson_sink.make(
-                ",".join(("", "crimson:sob-host={0}".format( sob ))),
-#                 ",".join(("", "")),
-                uhd.stream_args(
-                        cpu_format = "sc16",
-                        otw_format = "sc16",
-                        channels = (channels),
-                )
-        )
+        stream_args = uhd.stream_args( cpu_format = "fc32", otw_format = "sc16", channels = ( channels ) )
+        csrc = uhd.usrp_source( uhd.device_addr_t( "" ), stream_args, False )
+        csnk = uhd.usrp_sink( uhd.device_addr_t( "" ), stream_args )
+
+        # issue a stop to flush out the rx channels
+        sc = uhd.stream_cmd_t( uhd.stream_cmd_t.STREAM_MODE_STOP_CONTINUOUS )
+        csrc.issue_stream_cmd( sc )
 
         csrc.set_samp_rate( samp_rate )
-        csnk.set_samp_rate( samp_rate )
+        csrc.set_center_freq( freq )
+        csrc.set_gain( gain )
         
-        for ch in channels:
-            csrc.set_center_freq( freq, ch )
-            csrc.set_gain( gain, ch )
-            csnk.set_center_freq( freq, ch )
-            csnk.set_gain( gain, ch )
+        csnk.set_samp_rate( samp_rate )
+        csnk.set_center_freq( freq )
+        csnk.set_gain( gain )
 
         vsrc = blocks.vector_source_c( chirp )
+        vsrc.set_repeat( True )
         vsnk = blocks.vector_sink_c()
-        
-        co = blocks.complex_to_interleaved_short( True )
-        dec = blocks.interleaved_short_to_complex( True )
 
         ##################################################
         # Connections
         ##################################################
-        self.tb.connect( ( co, 0 ), ( csnk, 0 ) )
-        self.tb.connect( ( dec, 0 ), ( vsnk, 0 ) )
-        self.tb.connect( ( vsrc, 0 ), ( co, 0 ) )
-        self.tb.connect( ( csrc, 0 ), ( dec, 0 ) )    
-        
+        self.tb.connect( ( vsrc, 0 ), ( csnk, 0 ) )
+        self.tb.connect( ( csrc, 0 ), ( vsnk, 0 ) )
+
         ##################################################
         # Set Start of Burst
         ##################################################
+
+        if True:
+            time_now = uhd.time_spec_t.get_system_time()
+            csrc.set_time_now( time_now )
+        else:
+            time_now = csnk.get_time_now()
+        print( "Time now is {0}".format( time_now.get_real_secs() ) )
         
-        ## XXXX BROKEN XXXX
-        # XXX: @CF: 20171130: Unfortunately, via gr-uhd, this is passed through to the
-        # multi_crimson_tng::issue_stream_cmd() which is broken!!
+        start_time = uhd.time_spec_t( time_now.get_real_secs() + sob )
+        print( "Start Time is {0}".format( start_time.get_real_secs() ) )
         
-#         time_now = csnk.get_time_now()
-#         print( "Setting tx start time to {0}".format( (time_now + sob).get_real_secs() ) )
-#         csnk.set_start_time( time_now + sob )
-#         #csrc.set_start_time( time_now + sob )
-#         print( "Setting rx start time to {0}".format( (time_now + sob).get_real_secs() ) )
-#         sc = uhd.stream_cmd_t( uhd.stream_cmd_t.STREAM_MODE_NUM_SAMPS_AND_DONE )
-#         sc.num_samps = chirp_i.size
-#         sc.stream_now = False
-#         sc.time_spec = time_now + sob
-#         csrc.issue_stream_cmd( sc )
+        stop_time = uhd.time_spec_t( start_time.get_real_secs() + nsamples / samp_rate + 10 )
+        print( "Stop Time is {0}".format( stop_time.get_real_secs() ) )
+
+        # actually set tx start time
+        csnk.set_start_time( start_time )
+
+        # actually set rx start time
+        sc = uhd.stream_cmd_t( uhd.stream_cmd_t.STREAM_MODE_NUM_SAMPS_AND_DONE )
+        sc.num_samps = nsamples
+        sc.stream_now = False
+        sc.time_spec = start_time
+        csrc.issue_stream_cmd( sc )
+
+        killer = threading.Thread( group = None, target = self.tb_killer_fn, name = "killer", args = ( ( stop_time.get_real_secs() - start_time.get_real_secs() ), ) )
+        killer.start()
         
+        print( "Sending & Receiving {0} samples...".format( sc.num_samps ) )
+
         ##################################################
         # Run Flow Graph
         ##################################################
-        self.tb.run ()
+        time_now = csrc.get_time_now()
+        print( "Calling run. Time now is {0}".format( time_now.get_real_secs() ) )
+        self.tb.run()
+        time_now = csrc.get_time_now()
+        print( "returned from run. Time now is {0}".format( time_now.get_real_secs() ) )
         
         ##################################################
         # Verify Results
         ##################################################
-        
-        expected_data = tuple( map( tuple, chirp ) )
+
+        expected_data = tuple( chirp )
         actual_data = vsnk.data()
-        
-        self.assertTupleEqual( expected_data, actual_data )
-        self.assertEqual( len( expected_data ), len( actual_data ) )
+
+        self.assertComplexTuplesAlmostEqual2( expected_data, actual_data, 1.0/32768.0, 1.0/32768.0 );
 
 if __name__ == '__main__':
     gr_unittest.run(qa_crimson_source_sink_sob, "qa_crimson_source_sink_sob.xml")
